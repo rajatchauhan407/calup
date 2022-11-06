f07a428f1067eca8af1773b1c06c7509777fb1d8
Rajat Chauhan <rajatchauhan407@gmail.com>
2022-11-06 11:01:53 +0530

    corrected the domains

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ let BACKEND_DEV_URL;
 if(process.env.NODE_ENV === "development"){
     BACKEND_DEV_URL = "http://localhost:9000";
 }else if(process.env.NODE_ENV === "production"){
-    BACKEND_DEV_URL = "Calupbackend-env.eba-wxkj9sdg.us-east-1.elasticbeanstalk.com"
+    BACKEND_DEV_URL = "http://Calupbackend-env.eba-wxkj9sdg.us-east-1.elasticbeanstalk.com"
 }
 
 export {BACKEND_DEV_URL};
