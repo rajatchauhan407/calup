e30dae5cbf1d94ee312e434ed9ad6a6144d501e2
Rajat Chauhan <rajatchauhan407@gmail.com>
2022-11-09 09:26:08 +0530

    added ssl config and changed api

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ let BACKEND_DEV_URL;
 if(process.env.NODE_ENV === "development"){
     BACKEND_DEV_URL = "http://localhost:9000";
 }else if(process.env.NODE_ENV === "production"){
-    BACKEND_DEV_URL = "http://calupbackend-env.eba-wxkj9sdg.us-east-1.elasticbeanstalk.com"
+    BACKEND_DEV_URL = "https://server.calup.in"
 }
 
 export {BACKEND_DEV_URL};
